bb73f3596c6a387f37f016ca053bd0eb279f5360
CoetzeeNP <coetzeenp@ufs.ac.za>
2026-01-22 09:35:30 +0200

    readded thinking spinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from ai_strategy import AIManager
-from database import save_to_firebase, get_firebase_connection
+from database import save_to_firebase, get_firebase_connection, load_selected_chat
 from streamlit_cookies_controller import CookieController
 
 # 1. Initialize Cookie Controller before Page Config
